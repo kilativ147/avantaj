bf3871a02be23b9d83fb3d53b68dd34ebcd2bb7c
Vitalii <kilativ147@gmail.com>
2024-04-29 04:30:53 +0300

    Hero section - in progres
    Almost done, but need to add 'social' section and repair adaptability

---
+++ js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebP();
+
+// Тепер можливо підключати різні модулі через термінал, наприклад
+/*
+terminal: npm i swipper
+js: import Swiper, {Navigation, Pagination} from 'swiper';
+js: const swiper = new Swiper();
+
+Таким чином уникаючи вставляння посилань на бібліотеки та стилі таблиць
+*/
